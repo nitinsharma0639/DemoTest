52a46e6b6b4936e9865f282b696641a87d781719
Nitin <nitinsharma0639@gmail.com>
2024-10-14 23:53:08 +0530

    my first commit

---
+++ Hello.java
@@ -0,0 +1,14 @@
+public class Hello{
+
+public static int sum(int a , int b){
+    return a+b;
+}
+
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 10;
+       
+        System.out.println( sum(a, b));
+    }
+}
