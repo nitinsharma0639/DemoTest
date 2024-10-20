bb91f393791386cf17546665761f8a5a4204d0e6
Nitin <nitinsharma0639@gmail.com>
2024-10-20 22:24:39 +0530

    sub added

--- Hello.java
+++ Hello.java
@@ -6,7 +6,12 @@ public static int sum(int a , int b){
 
 public static int mul(int a , int b){
     return a*b;
-}    
+}  
+
+public static int sub(int a , int b){
+    return a-b;
+}   
+
 
     public static void main(String[] args) {
         
