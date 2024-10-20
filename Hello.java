e77a74aaf44572c29bc8122fa9840c69b742d408
Nitin <nitinsharma0639@gmail.com>
2024-10-20 23:20:22 +0530

    first commit

--- Hello.java
+++ Hello.java
@@ -6,11 +6,8 @@ public static int sum(int a , int b){
 
 public static int mul(int a , int b){
     return a*b;
-}  
-
-public static int sub(int a , int b){
-    return a-b;
-}   
+}
+ 
     public static void main(String[] args) {
         
         int a = 10;
